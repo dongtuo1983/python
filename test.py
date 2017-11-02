6072e55b7ef2b6c34247db94af3cc5af8b0dbcc1
dongtuo1983 <5471565@qq.com>
2017-11-02 18:18:43 +0800

    python

---
+++ test.py
@@ -0,0 +1,47 @@
+a = 100
+if a > 200:
+    print(a)
+else:
+    print(-a)
+
+print(10/3)
+
+print('I\'m \"ok\"')
+
+
+b = 12.3e10
+c = 1.23e11
+
+print(b+c)
+
+print("\\\n\\")
+
+print(r"\\\n\\")
+
+print('''line1
+line2
+line3''')
+
+b = c
+
+
+age = 10
+if age >= 18:
+    print("adult")
+else:
+    print("teenager")
+
+a = 1
+t_007 = "T007"
+Answer = True
+
+d = 123
+
+print(d)
+
+x = "abc"
+y = x
+x = "cdf"
+print(y)
+print(x)
+
